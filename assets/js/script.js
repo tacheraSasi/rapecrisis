b20b2d21f8ff87b5b3ab9d10e522dd456b2b3dbd
tacheraSasi <tacherasasi@gmail.com>
2024-06-24 22:51:05 +0300

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+const pataMsaada = document.getElementById('pata-msaada')
+const items = document.querySelectorAll(".item")
+
+items.forEach((item) =>{
+    item.addEventListener("click",(e)=>{
+        window.location.href = e.target.id
+    })
+})
+
+pataMsaada.addEventListener("click",()=>{
+    window.location.href = "pata-msaada"
+    console.log("clicked")
+})
